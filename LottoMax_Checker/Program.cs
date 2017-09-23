8cf1e1709cfe6649039d3f886aece0bd7d3b7772
Jordan Bussanich <jordan.bussanich@outlook.com>
2017-09-23 09:20:54 -0400

    Initial Commit

---
+++ LottoMax_Checker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LottoMax_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
